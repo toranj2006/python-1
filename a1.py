ebeb7a3f2e0005f7250251cacdafb1cc7fb187ad
user <user@users-MacBook-Pro.local>
2024-09-25 17:30:24 -0400

    init

---
+++ a1.py
@@ -0,0 +1,28 @@
+total = 0
+num_labs = int(input("How many labs did you complete?"))
+lab_weight = 0.2
+
+if num_labs > 6: 
+   num_labs = 6
+
+num_labs/6*10
+0
+total = total + (num_labs * lab_weight)_
+
+num_quizzes = int(input("How many quizzes did you complete?"))
+quizzes_weight = 0.15
+
+total = total + (num_quizzes * quizzes_weight)
+
+assignment_1 = int(input("What is your grade on assignment 1?"))
+assignment_1_weight = 0.04
+
+total = total + (assignment_1 * assignment_1_weight)
+
+assignment_2 = int(input("What is your grade on assignment 2"))
+assignment_2_weight = 0.04
+
+total = total + (assignment_2 * assignment_2_weight)
+
+
+
