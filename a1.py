57fff01cfa02f2d0bb90bbf3a8e910efe825ae06
user <user@dhcp-172-30-149-199.wireless.concordia.ca>
2024-09-27 18:49:46 -0400

    init

--- a1.py
+++ a1.py
@@ -6,7 +6,7 @@ lab_weight = 0.2
 if num_labs > 6:  
    num_labs = 6 
  
-total = total + (num_labs/6 * lab_weight) * 100 
+total += (num_labs/6 * lab_weight) * 100 
  
 num_quizzes = int(input("How many quizzes did you complete?")) 
 quizzes_weight = 0.15 
@@ -14,46 +14,46 @@ quizzes_weight = 0.15
 if num_quizzes > 6: 
    num_quizzes = 6 
  
-total = total + (num_quizzes/6 * quizzes_weight) * 100 
+total +=  (num_quizzes/6 * quizzes_weight) * 100 
  
 assignment_1 = int(input("What is your grade on Assignment 1?")) 
 assignment_1_weight = 0.04 
  
-total = total + (assignment_1 * assignment_1_weight) 
+total += (assignment_1 * assignment_1_weight) 
  
 assignment_2 = int(input("What is your grade on Assignment 2?")) 
 assignment_2_weight = 0.04 
  
-total = total + (assignment_2 * assignment_2_weight) 
+total += (assignment_2 * assignment_2_weight) 
  
 assignment_3 = int(input("What is your grade on Assignment 3?")) 
 assignment_3_weight = 0.04 
  
-total = total + (assignment_3 * assignment_3_weight) 
+total += (assignment_3 * assignment_3_weight) 
  
 assignment_4 = int(input("What is your grade on Assignment 4?")) 
 assignment_4_weight = 0.04 
  
-total = total + (assignment_4 * assignment_4_weight) 
+total +=  (assignment_4 * assignment_4_weight) 
  
 midterm_1 = int(input("What is your grade on the first midterm?")) 
 midterm_1_weight = 0.125 
  
-total = total + (midterm_1 * midterm_1_weight) 
+total +=  (midterm_1 * midterm_1_weight) 
  
 midterm_2 = int(input("What is your grade on the second midterm?")) 
 midterm_2_weight = 0.125 
  
-total = total + (midterm_2 * midterm_2_weight) 
+total += (midterm_2 * midterm_2_weight) 
  
 final_exam = int(input("What is your grade on the final exam?")) 
 final_exam_weight = 0.18 
  
-total = total + (final_exam * final_exam_weight) 
+total +=  (final_exam * final_exam_weight) 
  
 test_prep = int(input("What is your grade for the midterms and final preparation?")) 
 test_prep_weight = 0.06 
  
-total = total + (test_prep * test_prep_weight)  
+total += (test_prep * test_prep_weight)  
  
 print("Your final grade is:" + str(round(total,2)))
