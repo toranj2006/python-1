81bafb27f39da25b901317649fd6f4a27bcbc74d
toranj2006 <belletoranj2006@gmail.com>
2024-09-27 11:34:29 -0400

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,68 +1,62 @@
-total = 0
-
-num_labs = int(input("How many labs did you complete?"))
-lab_weight = 0.2
-
-if num_labs > 6: 
-   num_labs = 6
-
-num_labs/6*100
-
-total = total + (num_labs * lab_weight)
-
-num_quizzes = int(input("How many quizzes did you complete?"))
-quizzes_weight = 0.15
-
-if num_quizzes > 6:
-   num_quizzes = 6
-
-num_quizzes/6*100
-
-total = total + (num_quizzes * quizzes_weight)
-
-assignment_1 = int(input("What is your grade on assignment 1?"))
-assignment_1_weight = 0.04
-
-total = total + (assignment_1 * assignment_1_weight)
-
-assignment_2 = int(input("What is your grade on assignment 2?"))
-assignment_2_weight = 0.04
-
-total = total + (assignment_2 * assignment_2_weight)
-
-
-assignment_3 = int(input("What is your grade on assignment 3?"))
-assignment_3_weight = 0.04
-
-total = total + (assignment_3 * assignment_3_weight)
-
-assignment_4 = int(input("What is your grade on assignment 4?"))
-assignment_4_weight = 0.04
-
-total = total + (assignment_4 * assignment_4_weight)
-
-midterm_1 = int(input("What is your grade on the first midterm?"))
-midterm_1_weight = 0.125
-
-total = total + (midterm_1 * midterm_1_weight)
-
-midterm_2 = int(input("What is your grade on the second midterm?"))
-midterm_2_weight = 0.125
-
-total = total + (midterm_2 * midterm_2_weight)
-
-final_exam = int(input("What is your grade on the final exam?"))
-final_exam_weight = 0.18
-
-total = total + (final_exam * final_exam_weight)
-
-test_prep = int(input("What is your grade for the midterms and final preparation?"))
-test_prep_weight = 0.06
-
-total = total + (test_prep * test_prep_weight)
-
-total = total * 100
-final_grade = int(output("Your final grade is" + (total/6))
-
-
-
+total = 0 
+ 
+num_labs = int(input("How many labs did you complete?")) 
+lab_weight = 0.2 
+ 
+if num_labs > 6:  
+   num_labs = 6 
+ 
+total = total + (num_labs/6 * lab_weight) * 100 
+ 
+num_quizzes = int(input("How many quizzes did you complete?")) 
+quizzes_weight = 0.15 
+ 
+if num_quizzes > 6: 
+   num_quizzes = 6 
+ 
+total = total + (num_quizzes/6 * quizzes_weight) * 100 
+ 
+assignment_1 = int(input("What is your grade on assignment 1?")) 
+assignment_1_weight = 0.04 
+ 
+total = total + (assignment_1 * assignment_1_weight) 
+ 
+assignment_2 = int(input("What is your grade on assignment 2?")) 
+assignment_2_weight = 0.04 
+ 
+total = total + (assignment_2 * assignment_2_weight) 
+ 
+assignment_3 = int(input("What is your grade on assignment 3?")) 
+assignment_3_weight = 0.04 
+ 
+total = total + (assignment_3 * assignment_3_weight) 
+ 
+assignment_4 = int(input("What is your grade on assignment 4?")) 
+assignment_4_weight = 0.04 
+ 
+total = total + (assignment_4 * assignment_4_weight) 
+ 
+midterm_1 = int(input("What is your grade on the first midterm?")) 
+midterm_1_weight = 0.125 
+ 
+total = total + (midterm_1 * midterm_1_weight) 
+ 
+midterm_2 = int(input("What is your grade on the second midterm?")) 
+midterm_2_weight = 0.125 
+ 
+total = total + (midterm_2 * midterm_2_weight) 
+ 
+final_exam = int(input("What is your grade on the final exam?")) 
+final_exam_weight = 0.18 
+ 
+total = total + (final_exam * final_exam_weight) 
+ 
+test_prep = int(input("What is your grade for the midterms and final preparation?")) 
+test_prep_weight = 0.06 
+ 
+total = total + (test_prep * test_prep_weight) 
+ 
+total = total 
+ 
+print("Your final grade is" + str(total)) 
+ 
